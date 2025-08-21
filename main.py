a8649e47df5040b32c46edbd71fa1ed740d8f287
leraT2014 <alut2371@gmail.com>
2025-08-21 15:40:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import numpy as np
 from tensorflow.keras.models import load_model
 from PIL import Image, ImageOps
 from flask import Flask, request
+import re
 
 TOKEN = os.getenv("BOT_TOKEN")
 bot = telebot.TeleBot(TOKEN, parse_mode=None)
